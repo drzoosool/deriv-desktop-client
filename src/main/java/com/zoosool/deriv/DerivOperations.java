822b78ebfd732946b1abfe1255242292953ff1ad
zoosool <droginin@gmail.com>
2026-01-14 10:38:58 +0100

    Remove unused code

--- src/main/java/com/zoosool/deriv/DerivOperations.java
+++ src/main/java/com/zoosool/deriv/DerivOperations.java
@@ -2,8 +2,6 @@ package com.zoosool.deriv;
 
 import com.zoosool.model.Contract;
 
-import java.math.BigDecimal;
-
 public interface DerivOperations {
     void sell(Contract contract);
     void buy(Contract contract);
--- src/main/java/com/zoosool/deriv/NoOpDerivOperations.java
+++
@@ -1,9 +0,0 @@
-package com.zoosool.deriv;
-
-import com.zoosool.model.Contract;
-
-public class NoOpDerivOperations implements DerivOperations {
-    @Override public void buy(Contract contract) {}
-    @Override public void sell(Contract contract) {}
-    @Override public void buySell(Contract contract) {}
-}
